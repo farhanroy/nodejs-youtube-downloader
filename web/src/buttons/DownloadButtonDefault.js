cdee117d95109f64fc8b5bfddcc6aa17659701ea
farhanroy <farhanroy120@gmail.com>
2021-04-19 10:22:08 +0700

    :sparkles: built web for download youtube

---
+++ web/src/buttons/DownloadButtonDefault.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './DownloadButtonDefault.css';
+
+function DownloadButtonDefault() {
+    return (
+      <div className="default">
+        <h1>Paste the video url to get started..</h1>
+        <br />
+        <p>Copy url from youtube</p>
+      </div>
+    );
+  }
+  
+  export default DownloadButtonDefault;
